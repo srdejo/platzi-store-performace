73f9f49b3f866611532c32e8b94be1f0e16cd80f
Daniel Eduardo Jiménez Ovallos - Ceiba Software <daniel.jimenez@ceiba.com.co>
2021-10-04 16:08:08 -0500

    ng update @angular/core@11 @angular/cli@11

--- src/app/admin/components/form-product/form-product.component.spec.ts
+++ src/app/admin/components/form-product/form-product.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { FormProductComponent } from './form-product.component';
 
@@ -6,7 +6,7 @@ describe('FormProductComponent', () => {
   let component: FormProductComponent;
   let fixture: ComponentFixture<FormProductComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ FormProductComponent ]
     })
